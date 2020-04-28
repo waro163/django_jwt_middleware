f1715ff4385a97b51935fc6cab137d2144c45daa
waro163 <waro163@163.com>
2020-04-28 14:26:12 +0800

    fix the decode_jwt error

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ this project is for Django
 you must configure JWT_SECRET in the settings.py, eg.
 
 JWT_SECRET = os.environ.get("JWT_SECRET", 'b7y*ef&2a8j_ki&ms*c_o#!zp$!bgk8c=2f%^q@9y16zntsp=2')
+
+MIDDLEWARE = [
+    'django_jwt_middleware.middleware.jwt_middleware.JWTMiddleware',
+    ...
+]
--- django_jwt_middleware/utils/decode_jwt.py
+++ django_jwt_middleware/utils/decode_jwt.py
@@ -4,7 +4,7 @@ from .get_settings_value import get_setting_value
 def decode_jwt(token,algorithms='HS256'):
     jwt_secret = get_setting_value('JWT_SECRET')
     try:
-        jwt_payload = jwt.decode(token, secret, algorithms=[algorithms])
+        jwt_payload = jwt.decode(token, jwt_secret, algorithms=[algorithms])
     except Exception as e:
         raise e
     return jwt_payload
